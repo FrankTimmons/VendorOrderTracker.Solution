0e62792d3cec62640b5dcd3c21bdc7d94d217d47
FrankTimmons <franktimmonspdx@gmail.com>
2022-07-22 11:30:01 -0700

    Pass fourth test for vendor class by adding IDisposable.

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,8 +6,14 @@ using VendorOrderTracker.Models;
 namespace VendorOrderTracker.Tests 
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    
     [TestMethod]
     public void VendorConstructor_CreateVendorInstance_Vendor()
     {
