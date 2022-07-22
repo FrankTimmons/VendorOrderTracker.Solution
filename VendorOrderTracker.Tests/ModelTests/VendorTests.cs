d8809a5bada828e06eda81dad0e5ca2d3756713b
FrankTimmons <franktimmonspdx@gmail.com>
2022-07-22 11:29:20 -0700

    Good fail fourth test for vendor class.

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
 
-namespace VendorOrderTracker.Tests
+namespace VendorOrderTracker.Tests 
 {
   [TestClass]
   public class VendorTests
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -5,6 +5,7 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
@@ -12,6 +13,12 @@ namespace VendorOrderTracker.Models
     {
       Name = name;
       Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
